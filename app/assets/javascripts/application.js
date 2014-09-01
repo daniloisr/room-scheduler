664c2a9f553bca5a918c5fc17e37437f24d246f5
Danilo Resende <danilo.isr@gmail.com>
2014-09-01 16:19:28 -0300

    refactor scheduler view code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+  $('.scheduler .schedulable').on('click', function(ev) {
+    var element = $(this);
+    $.post('/scheduler', element.data(), function(json) {
+      element.html($('<span class="label label-success">').html(json.message));
+    }, 'json');
+
+    ev.preventDefault();
+  });
+});
--- app/helpers/schedulers_helper.rb
+++ app/helpers/schedulers_helper.rb
@@ -5,4 +5,11 @@ module SchedulersHelper
       content_tag(:span, l(Week.at(wday), format: :day_month), class: 'text-muted')
     ).html_safe
   end
+
+  def schedule_label(schedule)
+    klass = schedule.user == current_user ? 'success' : 'warning'
+    content_tag(:span,
+      "Reservado por #{schedule.user.name}",
+      class: "label label-#{klass}").html_safe
+  end
 end
